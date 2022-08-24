16a41a40c464d302a9720326094e658aa2739372
demVadim <demmed85@mail.ru>
2022-08-24 09:42:16 +0800

    init css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log("JavaScript");
+function ask(question, yes, no) {
+if ( confirm(question) ) 
+{
+	yes();
+}
+else no();
+}
+ask(
+"Вы согласны?",
+function() { alert("Вы согласились."); },
+function() { alert("Вы отменили выполнение."); }
+);
+
+
