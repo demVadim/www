a3192248e88ffeeffc2d39b2634a7afc10602e1f
demVadim <demmed85@mail.ru>
2024-01-25 17:00:02 +0800

    add rect

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-function ask(question, yes, no) {
-if ( confirm(question) ) 
-{
-	yes();
-}
-else no();
-}
-ask(
-"Вы согласны?",
-function() { alert("Вы согласились."); },
-function() { alert("Вы отменили выполнение."); }
-);
+var canvas = document.getElementById("canvasId");
+var ctx = canvas.getContext("2d");
+canvas.width = 200;
+canvas.height = 90;
+ctx.strokeStyle = "green";
+ctx.fillStyle = "blue";
+ctx.shadowBlur = 10;
+ctx.shadowColor = "brown";
+
+ctx.rect(10,30,80,40);
+ctx.fill();
+ctx.stroke();
 
 
